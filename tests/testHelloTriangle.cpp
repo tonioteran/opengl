a3300878a51471a9f8999418316a8fb4c4641450
antonio teran <teran@mit.edu>
2018-12-30 00:52:03 +0100

    add excercies 1

--- tests/testHelloTriangle.cpp
+++ tests/testHelloTriangle.cpp
@@ -89,24 +89,58 @@ int main(void) {
   }
   glDeleteShader(vertexShader); glDeleteShader(fragmentShader);
 
-
+  // -- set up vertex data
+  float vertices[] = {
+    -0.8f, -0.5f, 0.0f, 
+     0.5f, -0.5f, 0.0f,
+     0.0f,  0.5f, 0.0f,
+     0.5f,  0.0f, 0.0f,
+     0.7f,  0.2f, 0.0f,
+     0.2f,  0.8f, 0.0f
+  };
+  
+  // -- configure vertex attributes
   /* vertex buffer object */
-  unsigned int VBO;
+  unsigned int VBO, VAO;
+  glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+
+  // -- bind vertex array first, then bind and set vertex buffers
+  glBindVertexArray(VAO);
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // -- configur vertex attributes
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+
+  glBindBuffer(GL_ARRAY_BUFFER, 0);
+  glBindVertexArray(0);
+
+  glPolygonMode(GL_FRONT_AND_BACK, GL_LINE); // to draw wireframe
 
   /* render loop */
   while ( !glfwWindowShouldClose(window) ) {
 
     processInput(window);
-
+    
+    // -- render
     glClearColor(0.2f, 0.2f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // -- draw triangle
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
 
   }
+
+  // -- deallocate resources
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
 
   glfwTerminate();
 

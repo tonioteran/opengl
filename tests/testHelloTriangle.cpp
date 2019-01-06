12c1679a74fd75a065e6d5e1d278b23719cde6ad
antonio teran <teran@mit.edu>
2019-01-05 22:45:17 -0500

    triangle test

--- tests/testHelloTriangle.cpp
+++ tests/testHelloTriangle.cpp
@@ -101,16 +101,16 @@ int main(void) {
   
   // -- configure vertex attributes
   /* vertex buffer object */
-  unsigned int VBO, VAO;
-  glGenVertexArrays(1, &VAO);
-  glGenBuffers(1, &VBO);
+  unsigned int VBO[2], VAO[2];
+  glGenVertexArrays(2, VAO);
+  glGenBuffers(2, VBO);
 
   // -- bind vertex array first, then bind and set vertex buffers
-  glBindVertexArray(VAO);
-  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBindVertexArray(VAO[0]);
+  glBindBuffer(GL_ARRAY_BUFFER, VBO[0]);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  // -- configur vertex attributes
+  // -- configure vertex attributes
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
 
@@ -132,6 +132,8 @@ int main(void) {
     glUseProgram(shaderProgram);
     glBindVertexArray(VAO);
     glDrawArrays(GL_TRIANGLES, 0, 6);
+
+    glBindVertexArray(0); // unbind 
 
     glfwSwapBuffers(window);
     glfwPollEvents();

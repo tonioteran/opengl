9b8c470c8257b6abd426fa247a7231fb1fabe5e8
antonio teran <teran@mit.edu>
2019-02-12 20:58:38 -0500

    add and finish transform demo

---
+++ apps/transformDemo.cpp
@@ -0,0 +1,186 @@
+/**
+ * @file transformDemo.cpp
+ * @brief demo that shows how to apply transformations to objects
+ * @date Feb 12, 2019
+ * @author Antonio Teran (teran@mit.edu)
+ */
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <iostream>
+#include "Shader.h"
+
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH  = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main(void) {
+
+  std::cout << "Texture tests!" << std::endl;
+
+  // initialize
+  glfwInit();
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // create the window
+  GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT,
+                                        "HelloWindow", NULL, NULL);
+  if ( window == NULL ) {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return (-1);
+  }
+  glfwMakeContextCurrent(window);
+
+  // load OpenGL function pointers
+  if ( !gladLoadGLLoader((GLADloadproc)glfwGetProcAddress) ) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return (-1);
+  }
+
+  // load shaders from source file
+  Shader ourShader("/home/tonio/repos/opengl/apps/transformShader.vs",
+                   "/home/tonio/repos/opengl/apps/transformShader.fs");
+
+  // setup vertex data for the rectangle
+  float vertices[] = {
+    // positions        // texture coords
+    0.5f,  0.5f, 0.0f,  1.0f, 1.0f, // top right
+    0.5f, -0.5f, 0.0f,  1.0f, 0.0f, // bottom right
+   -0.5f, -0.5f, 0.0f,  0.0f, 0.0f, // bottom left
+   -0.5f,  0.5f, 0.0f,  0.0f, 1.0f  // top left 
+  };
+  unsigned int indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+  };
+
+  // generate buffer and arrays for the 2 triangles (one square)
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  glBindVertexArray(VAO);
+
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  // position attribute
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5*sizeof(float), (void*)0);
+  glEnableVertexAttribArray(0);
+  // texture attribute
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5*sizeof(float),
+                        (void*)(3*sizeof(float)) );
+  glEnableVertexAttribArray(1);
+
+  // load and create a texture
+  unsigned int texture1, texture2;
+
+  glGenTextures(1, &texture1);
+  glBindTexture(GL_TEXTURE_2D, texture1);
+  // set wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load image, create texture and generate mipmaps
+  int width, height, nr_channels;
+  stbi_set_flip_vertically_on_load(true);
+  std::string container_path =
+    "/home/tonio/repos/opengl/resources/textures/container.jpg";
+  unsigned char *data = stbi_load(container_path.c_str(),
+                                  &width, &height, &nr_channels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB,
+                 width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else { std::cout << "Failed to load containter texture" << std::endl; }
+  stbi_image_free(data);
+
+  glGenTextures(1, &texture2);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+  // set wrapping parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  // set filtering parameters
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  // load image, create texture and generate mipmaps
+  std::string face_path =
+    "/home/tonio/repos/opengl/resources/textures/awesomeface.png";
+  data = stbi_load(face_path.c_str(),
+                                  &width, &height, &nr_channels, 0);
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA,
+                 width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else { std::cout << "Failed to load awesomeface texture" << std::endl; }
+  stbi_image_free(data);
+
+  // activate the shader before setting uniforms!
+  ourShader.use();
+  glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+  ourShader.setInt("texture2", 1);
+
+
+  // render loop:
+  while (!glfwWindowShouldClose(window)) {
+    // input
+    processInput(window);
+
+    //render
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // bind textures on corresponding texture units
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    // create tranformations
+    glm::mat4 transform = glm::mat4(1.0f); // init as identity
+    transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+    transform = glm::rotate(transform, (float)glfwGetTime(),
+                            glm::vec3(0.0f, 0.0f, 1.0f));
+
+    // get matrix uniform location and set matrix
+    ourShader.use();
+    unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+    glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));
+
+    // render container
+    glBindVertexArray(VAO);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+
+  }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+
+  glfwTerminate();
+  return (0);
+
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    glfwSetWindowShouldClose(window, true);
+}
